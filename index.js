0e62f9f06b08332a6c617196000a33be56b43c26
AbuEskander <basselawni1@gmail.com>
2023-10-18 23:12:44 +0300

    Linked Files Together

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import {dirname} from "path"
 import { fileURLToPath } from 'url';
 const app = express();
 const port = 3000;
+app.use(express.static("public"))
 
+app.get("/",(req,res)=>{
+    res.render("index.ejs")
+})
 app.listen(port ,()=>{
     console.log(`Server is running on ${port}`);
 })
