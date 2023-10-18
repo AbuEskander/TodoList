c721437db710aa81ab487a578866984900cfdc49
AbuEskander <basselawni1@gmail.com>
2023-10-18 23:50:57 +0300

    Todo list functionlaity is preliminary Done

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ import {dirname} from "path"
 import { fileURLToPath } from 'url';
 const app = express();
 const port = 3000;
-app.use(express.static("public"))
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+let TodoW =[];
+let Work =[];
 
 app.get("/",(req,res)=>{
     res.render("index.ejs")
+})
+app.post("/submit",(req,res)=>{
+    TodoW.push(req.body.Todo);
+    res.render("index.ejs",{Todo:TodoW});
 })
 app.listen(port ,()=>{
     console.log(`Server is running on ${port}`);
