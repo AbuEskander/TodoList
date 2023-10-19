b1a77d83742a71eecd8c094e45a8762d7b0dc79b
AbuEskander <basselawni1@gmail.com>
2023-10-19 23:12:24 +0300

    Linked BootStrap & Done some Work on the logic + Some Front End Work

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import {dirname} from "path"
-import { fileURLToPath } from 'url';
+import {
+    dirname
+} from "path"
+import {
+    fileURLToPath
+} from 'url';
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-let TodoW =[];
-let Work =[];
+let TodoW = [];
+let Work = [];
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index.ejs")
 })
-app.post("/submit",(req,res)=>{
-    TodoW.push(req.body.Todo);
-    res.render("index.ejs",{Todo:TodoW});
+app.post("/submit", (req, res) => {
+    if (req.body.Todo.length) {
+        TodoW.push(req.body.Todo);
+        res.render("index.ejs", {
+            Todo: TodoW
+        });
+    } else {
+        if (TodoW.length) {
+            res.render("index.ejs", {
+                Todo: TodoW
+            })
+        } else {
+            res.render("index.ejs")
+        }
+    }
+
 })
-app.listen(port ,()=>{
+app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 })
