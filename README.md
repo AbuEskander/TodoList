c8e94de25907e72210bf1acf0ec98e4ce0ca6e87
AbuEskander <basselawni1@gmail.com>
2023-11-10 23:49:09 +0300

    A readme file is done

---
+++ README.md
@@ -0,0 +1,9 @@
+# Todo List application 
+a Web application that tracks the list of todos I am planning to do!
+
+#### in Frontend:
+    I used BootStrap mainly as the framework for me
+#### in backend:
+    I used Ejs, expressJS, bodyParser and all of that ran on Node.js
+
+## Youtube URL: https://youtu.be/P0TxMEVi7BY
--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-import {
-    dirname
-} from "path"
-import {
-    fileURLToPath
-} from 'url';
+import bodyParser from "body-parser";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 let TodoW = [];
 let Work = [];
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
-})
+  res.render("index.ejs");
+});
 app.post("/submit", (req, res) => {
-    if (req.body.Todo.length) {
-        TodoW.push(req.body.Todo);
-        res.render("index.ejs", {
-            Todo: TodoW
-        });
+  if (req.body.Todo.length) {
+    TodoW.push(req.body.Todo);
+    res.render("index.ejs", {
+      Todo: TodoW,
+    });
+  } else {
+    if (TodoW.length) {
+      res.render("index.ejs", {
+        Todo: TodoW,
+      });
     } else {
-        if (TodoW.length) {
-            res.render("index.ejs", {
-                Todo: TodoW
-            })
-        } else {
-            res.render("index.ejs")
-        }
+      res.render("index.ejs");
     }
-
-
-})
-app.post("/delete",(req,res)=>{
-    TodoW.pop("Hey")
-    res.render("index.ejs",{Todo:TodoW})
-})
+  }
+});
+app.post("/delete", (req, res) => {
+  TodoW.pop("Hey");
+  res.render("index.ejs", {
+    Todo: TodoW,
+  });
+});
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
-})
+  console.log(`Server is running on ${port}`);
+});
